fd18b4de37050cce927dcfeae81ccacd1b7cae74
JobzzOff <jobzz.off@gmail.com>
2022-05-26 23:05:08 +0300

    Added Dockerfile. Server can be started within a container

--- server/main.go
+++ server/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+func simpleHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	response := fmt.Sprintf("Test %s", id)
+	fmt.Fprint(w, response)
+}
 
 func main() {
 	router := mux.NewRouter()
-	err := http.ListenAndServe("127.0.0.1:8080", router)
+	router.HandleFunc("/test/{id:[0-9]+}", simpleHandler)
+	log.Println("Server started. Listening...")
+	err := http.ListenAndServe(":8181", router)
 	if err != nil {
 		log.Fatalf("failed to listen and serve: %s", err.Error())
 		panic("failed to listen and serve: " + err.Error())
