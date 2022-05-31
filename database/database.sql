aa7a551175b3199c12f0c0551f18a96b724d6b4d
VladimirBalun <vladimirbalun@yandex.ru>
2022-05-31 13:45:44 +0300

    cpa-network-1: add initial database schema

--- database/database.sql
+++ database/database.sql
@@ -1,1 +1,263 @@
-CREATE DATABASE cpa-network;
+-- MySQL dump 10.13  Distrib 8.0.29, for macos11.6 (arm64)
+--
+-- Host: localhost    Database: cpa_network
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `advertiser`
+--
+
+DROP TABLE IF EXISTS `advertiser`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `advertiser` (
+  `advertiser_id` binary(16) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`advertiser_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `advertiser`
+--
+
+LOCK TABLES `advertiser` WRITE;
+/*!40000 ALTER TABLE `advertiser` DISABLE KEYS */;
+/*!40000 ALTER TABLE `advertiser` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `city`
+--
+
+DROP TABLE IF EXISTS `city`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `city` (
+  `city_id` binary(16) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`city_id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `city`
+--
+
+LOCK TABLES `city` WRITE;
+/*!40000 ALTER TABLE `city` DISABLE KEYS */;
+/*!40000 ALTER TABLE `city` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `offer`
+--
+
+DROP TABLE IF EXISTS `offer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `offer` (
+  `offer_id` binary(16) NOT NULL,
+  `offer_type_id` tinyint NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `city_id` binary(16) NOT NULL,
+  `payment_per_company` float NOT NULL,
+  `payment_per_master` float NOT NULL,
+  PRIMARY KEY (`offer_id`),
+  KEY `offer_with_offer_type_fk` (`offer_type_id`),
+  KEY `offer_with_city_fk` (`city_id`),
+  CONSTRAINT `offer_with_city_fk` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`),
+  CONSTRAINT `offer_with_offer_type_fk` FOREIGN KEY (`offer_type_id`) REFERENCES `offer_type` (`offer_type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `offer`
+--
+
+LOCK TABLES `offer` WRITE;
+/*!40000 ALTER TABLE `offer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `offer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `offer_type`
+--
+
+DROP TABLE IF EXISTS `offer_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `offer_type` (
+  `offer_type_id` tinyint NOT NULL,
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`offer_type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `offer_type`
+--
+
+LOCK TABLES `offer_type` WRITE;
+/*!40000 ALTER TABLE `offer_type` DISABLE KEYS */;
+INSERT INTO `offer_type` VALUES (1,'PLUMBING'),(2,'ELECTRICIAN'),(3,'MASTER_FOR_AN_HOUR');
+/*!40000 ALTER TABLE `offer_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `partner`
+--
+
+DROP TABLE IF EXISTS `partner`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `partner` (
+  `id` binary(16) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `partner`
+--
+
+LOCK TABLES `partner` WRITE;
+/*!40000 ALTER TABLE `partner` DISABLE KEYS */;
+/*!40000 ALTER TABLE `partner` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ticket`
+--
+
+DROP TABLE IF EXISTS `ticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ticket` (
+  `ticket_id` binary(16) NOT NULL,
+  `client_id` binary(16) DEFAULT NULL,
+  `date` date NOT NULL,
+  `action_id` tinyint NOT NULL,
+  `type_id` tinyint NOT NULL,
+  `offer_id` binary(16) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `ticket_status_id` tinyint NOT NULL,
+  `city_id` binary(16) NOT NULL,
+  PRIMARY KEY (`ticket_id`),
+  KEY `ticket_with_ticket_type_fk` (`type_id`),
+  KEY `ticket_with_offer_fk` (`offer_id`),
+  KEY `ticket_with_ticket_status_fk` (`ticket_status_id`),
+  KEY `ticket_with_city_fk` (`city_id`),
+  CONSTRAINT `ticket_with_offer_fk` FOREIGN KEY (`offer_id`) REFERENCES `offer` (`offer_id`),
+  CONSTRAINT `ticket_with_ticket_status_fk` FOREIGN KEY (`ticket_status_id`) REFERENCES `ticket_status` (`ticket_status_id`),
+  CONSTRAINT `ticket_with_city_fk` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`),
+  CONSTRAINT `ticket_with_ticket_type_fk` FOREIGN KEY (`type_id`) REFERENCES `ticket_type` (`ticket_type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ticket`
+--
+
+LOCK TABLES `ticket` WRITE;
+/*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ticket_action`
+--
+
+DROP TABLE IF EXISTS `ticket_action`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ticket_action` (
+  `ticket_action_id` tinyint NOT NULL,
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`ticket_action_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ticket_action`
+--
+
+LOCK TABLES `ticket_action` WRITE;
+/*!40000 ALTER TABLE `ticket_action` DISABLE KEYS */;
+INSERT INTO `ticket_action` VALUES (1,'PHONE_CALL'),(2,'FORM_SUBMIT'),(3,'CLICK');
+/*!40000 ALTER TABLE `ticket_action` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ticket_status`
+--
+
+DROP TABLE IF EXISTS `ticket_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ticket_status` (
+  `ticket_status_id` tinyint NOT NULL,
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`ticket_status_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ticket_status`
+--
+
+LOCK TABLES `ticket_status` WRITE;
+/*!40000 ALTER TABLE `ticket_status` DISABLE KEYS */;
+INSERT INTO `ticket_status` VALUES (1,'ACCEPTED'),(2,'PENDING'),(3,'REJECTED');
+/*!40000 ALTER TABLE `ticket_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ticket_type`
+--
+
+DROP TABLE IF EXISTS `ticket_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ticket_type` (
+  `ticket_type_id` tinyint NOT NULL,
+  `description` varchar(100) NOT NULL,
+  PRIMARY KEY (`ticket_type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ticket_type`
+--
+
+LOCK TABLES `ticket_type` WRITE;
+/*!40000 ALTER TABLE `ticket_type` DISABLE KEYS */;
+INSERT INTO `ticket_type` VALUES (1,'PRIVATE_MASTER'),(2,'COMPANY');
+/*!40000 ALTER TABLE `ticket_type` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-05-31 13:38:01
