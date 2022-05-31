71229afa9eddac2d63b05dd71c196b569102b5e7
Alex Scherba <alexscherba16@gmail.com>
2022-05-31 21:56:07 +0300

    cpa-network-4: add handler endpoints

---
+++ server/internal/transport/http/api.go
@@ -0,0 +1,63 @@
+package http
+
+import "net/http"
+
+// GetMain - retrieves main page content
+func (h *Handler) GetMain(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+// GetOffers - retrieves offers page content
+func (h *Handler) GetOffers(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+// GetThreads - retrieves threads page content
+func (h *Handler) GetThreads(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+// GetCreatives - retrieves creatives page content
+func (h *Handler) GetCreatives(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+// GetDomains - retrieves domains page content
+func (h *Handler) GetDomains(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+// GetFinances - retrieves finances page content
+func (h *Handler) GetFinances(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+// GetStatistics - retrieves statistics page content
+func (h *Handler) GetStatistics(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+// GetInstruments - retrieves instruments page content
+func (h *Handler) GetInstruments(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+// GetHelp - retrieves help page content
+func (h *Handler) GetHelp(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+// GetApi - retrieves api page content
+func (h *Handler) GetApi(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+// GetPostbacks - retrieves postbacks page content
+func (h *Handler) GetPostbacks(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+// GetUsefulServices - retrieves useful services page content
+func (h *Handler) GetUsefulServices(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
