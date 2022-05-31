f439dec2a2e7f33627186f5198a00cdb6140fe6c
Alex Scherba <alexscherba16@gmail.com>
2022-05-31 21:58:29 +0300

    cpa-network-4: setup handler endpoints routes

--- server/main.go
+++ server/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
+	transportHTTP "cpa_network/internal/transport/http"
 	"log"
 	"net/http"
 )
 
 func main() {
-	router := mux.NewRouter()
-	err := http.ListenAndServe("127.0.0.1:8080", router)
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	err := http.ListenAndServe("127.0.0.1:8080", handler.Router)
 	if err != nil {
 		log.Fatalf("failed to listen and serve: %s", err.Error())
 		panic("failed to listen and serve: " + err.Error())
